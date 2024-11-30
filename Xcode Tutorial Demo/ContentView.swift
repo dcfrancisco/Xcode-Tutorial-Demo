f62ef0a1f8209606810a559cfaf7d97fa9cb78df
Danny Francisco <dcfran@gmail.com>
2024-11-30 19:14:41 +0800

    Initial Commit

---
+++ Xcode Tutorial Demo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Xcode Tutorial Demo
+//
+//  Created by Danny Francisco on 11/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Xcode Tutorial Demo/Xcode_Tutorial_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode_Tutorial_DemoApp.swift
+//  Xcode Tutorial Demo
+//
+//  Created by Danny Francisco on 11/30/24.
+//
+
+import SwiftUI
+
+@main
+struct Xcode_Tutorial_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
